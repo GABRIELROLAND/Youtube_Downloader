2e303fa3d6161a4c461833d463d59367bc5607a6
Guilherme Mendes <guilhermemendes789@gmail.com>
2018-07-25 16:11:23 -0300

    Opção de baixar direto de uma url

--- Downloader.py
+++ Downloader.py
@@ -1,4 +1,3 @@
-
 from pytube import YouTube
 from bs4 import BeautifulSoup
 import requests
@@ -11,9 +10,12 @@ def download_yt(url):
     #print(yt.streams.filter(only_audio=False).all())
     choice = input('Digite a para áudio e v para vídeo: ')
     if choice == 'a':
+        print('Baixando áudio de '+url)
         yt.streams.get_by_itag(140).download()
     else:
+        print('Baixando video de '+url)
         yt.streams.get_by_itag(137).download()
+    print('~ Download Completo ~')
 
 # def get_extension(url):
 #     if 'youtube' in url:
@@ -30,18 +32,21 @@ def download_yt(url):
 #     return file_name
 
 def main():
-    musica = input('Digite a música a ser baixada: ')
-    musica = musica.replace(" ","+")
-    source = requests.get('https://www.youtube.com/results?search_query='+musica).text
-    soup = BeautifulSoup(source,"html5lib")
-    count=0
-    vetor=[]
-    for link in soup.find_all('div',class_="yt-lockup-content"):
-        download_link = link.find('a')['href']
-        vetor.insert(count,('youtube.com'+download_link))
-        print(str(count)+"-"+'Nome: '+link.h3.a.text)
-        print('Link: youtube.com'+download_link+'\n')
-        count +=1
-    escolha = int(input('Escolha a opção para fazer download: '))
-    download_yt(vetor[escolha])
+    musica = input('Digite a música ou link a ser baixado do Youtube: ')
+    if 'youtube.com' in musica:
+        download_yt(musica)
+    else:
+        musica = musica.replace(" ","+")
+        source = requests.get('https://www.youtube.com/results?search_query='+musica).text
+        soup = BeautifulSoup(source,"html5lib")
+        count=0
+        vetor=[]
+        for link in soup.find_all('div',class_="yt-lockup-content"):
+            download_link = link.find('a')['href']
+            vetor.insert(count,('youtube.com'+download_link))
+            print(str(count)+"-"+'Nome: '+link.h3.a.text)
+            print('Link: youtube.com'+download_link+'\n')
+            count +=1
+        escolha = int(input('Escolha a opção para fazer download: '))
+        download_yt(vetor[escolha])
 main()
